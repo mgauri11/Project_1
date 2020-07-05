aad65791814d06843549e17dd679c80c152de782
mgauri2109 <mudgauri11@gmail.com>
2020-07-05 18:03:48 +0100

    Updated frontend and backend folders

--- Express-backend/routes/index.js
+++ Express-backend/routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-
-router.get('/ping', function(req, res){
-    res.status(200).send("pong!");
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- React-frontend/src/App.js
+++ React-frontend/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {Route,Switch} from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Login from './components/login';
-import Register from './components/register';
-import Home from './components/Home';
-
-const App = () => {
-
-  
-  
+//
+function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path='/login' component={Login}></Route>
-        <Route exact path='/register' component={Register}></Route>
-        <Route exact path='/' component={Home}></Route>
-
-      </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
