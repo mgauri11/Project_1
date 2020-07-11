162b9da6162439be9a56a077eb9b7e26490e22f9
mgauri2109 <mudgauri11@gmail.com>
2020-07-11 21:56:28 +0100

    Added login authentication and mongodb logic for backend

--- Express-backend/app.js
+++ Express-backend/app.js
@@ -1,41 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const passport = require("./passport/setup");
+const auth = require("./routes/auth");
 
-var app = express();
+const app = express();
+const PORT = 5000;
+const MONGO_URI = "mongodb://127.0.0.1:27017/tutorial_social_login";
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true })
+    .then(console.log(`MongoDB connected ${MONGO_URI}`))
+    .catch(err => console.log(err));
 
-app.use(logger('dev'));
+// Bodyparser middleware, extended false does not allow nested payloads
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Express Session
+app.use(
+    session({
+        secret: "very secret this is",
+        resave: false,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    })
+);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Routes
+app.use("/api/auth", auth);
+app.get("/", (req, res) => res.send("Good monring sunshine!"));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(PORT, () => console.log(`Backend listening on port ${PORT}!`));
+
 
 module.exports = app;
